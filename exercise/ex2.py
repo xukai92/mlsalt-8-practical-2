ed0f34f7d8f52232b1498093a0f2ad96746b1788
K. Xu <kx216@mlsalt-cpu1.eng.cam.ac.uk>
2016-03-17 02:11:34 +0000

    remove the last line of ex1.py

--- exercise/ex2.py
+++ exercise/ex2.py
@@ -1,10 +1,6 @@
 import os
 
 
-os.system('HIFST=/home/wjb31/src/hifst.mlsalt-cpu1.5Nov15/ucam-smt/')
-os.system('export PATH=$PATH:$HIFST/bin/')
-os.system('export PATH=$PATH:$HIFST/externals/openfst-1.5.0/INSTALL_DIR/bin/')
-
 # 1a.fst - a letter in L (including space)
 # 1b.fst - a single space
 # 1c.fst - a capitalized word
