16b2e97b8c00ee60aac3eb757db8e4158a229bc5
K. Xu <kx216@mlsalt-cpu1.eng.cam.ac.uk>
2016-03-17 02:34:52 +0000

    fix bug in 1d

--- exercise/ex1.py
+++ exercise/ex1.py
@@ -55,7 +55,7 @@ out += '0 1 a a\n'
 for s in L:
     if s != '<eps>' and s != '<space>':
         if s != 'a':
-            out += '0 1 {s} {s}\n'.format(s=s)
+            out += '0 0 {s} {s}\n'.format(s=s)
         out += '1 1 {s} {s}\n'.format(s=s)
 out += '1'
 f_out.write(out)
